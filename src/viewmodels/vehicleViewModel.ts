41de90e140c11cadc4d603d725c0e75a053c8a9b
Manticore-007 <71702374+Manticore-007@users.noreply.github.com>
2025-05-23 13:33:17 +0200

    fixed `setSequence` function

--- src/viewmodels/vehicleViewModel.ts
+++ src/viewmodels/vehicleViewModel.ts
@@ -363,13 +363,8 @@ export class VehicleViewModel
 	/**
 	 * Toggle the visibilty of specific vehicles options.
 	 */
-	_setSequence(index: number): void
-	{
-		if (index === CopyOptions.SpecificVehiclesOnTrain)
-		{
-			this._isSequence.set(true);
-		}
-		this._isSequence.set(false);
+	_setSequence(index: number): void {
+		this._isSequence.set(index === CopyOptions.SpecificVehiclesOnTrain);
 	}
 
 	/**
